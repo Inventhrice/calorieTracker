27f85e24b5a24ba719f5c810436086fbf730fedf
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2025-06-30 00:11:00 -0400

    fix: trying to make it so the start date defaults to this week monday.

--- public/components/entriesDatePicker.js
+++ public/components/entriesDatePicker.js
@@ -29,13 +29,14 @@ export default {
         getLocalDate(date = undefined) {
             if (date === undefined) {
                 date = new Date()
-                date.setUTCHours(8)
+                date.setHours(8)
             }
             return date.toISOString().split('T')[0]
         },
         getLastMon(date) {
             // Find the difference of dates till the monday, and get the last Monday that has occured in the week. If the day is the same (1==1), then -1*0 is 9, nothing is added.
             const MONDAY = 1
+            date.setHours(8)
             let diffStartToMonday = date.getDay() - MONDAY
             return this.addDate(date, -1 * diffStartToMonday)
         },
