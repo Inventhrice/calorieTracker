74415b2eb629468374a547c370f01e376fc9ac6b
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2025-07-01 22:18:41 -0400

    refactor: moving all the sidebar stuff into it's actual function

--- public/components/sidebarItem.js
+++ public/components/sidebarItem.js
@@ -11,11 +11,14 @@ export default {
             ]
         }
     },
-    template: `<div class="sidebar">
-    <a v-for="icon in sidebarIcons" :href=icon.link>
-        <div class="sidebar-icon group">
-            <span class="iconify" :data-icon="'mdi-' + icon.icon"></span>
-            <span class="sidebar-tooltip group-hover:scale-100">{{icon.text}}</span>
-        </div>
-    </a></div>`
+    template: `
+<div class="flex flex-col w-auto h-screen p-2 bg-white dark:bg-gray-900 shadow-lg">
+    <span v-for="icon in sidebarIcons" >
+		<a :href=icon.link class="relative flex items-center justify-center w-auto m-1 p-3 bg-gray-400 hover:bg-blue-600 dark:bg-gray-800 text-blue-500 hover:text-white hover:rounded-xl rounded-3xl transition-all duration-300 ease-linear cursor-pointer shadow-lg group">
+			<span class="iconify" :data-icon="'mdi-' + icon.icon"></span>
+			<span class="absolute p-2 m-3 min-w-max left-14 rounded-md shadow-md z-50 text-white bg-gray-900 text-xs font-bold transition-all duration-100 scale-0 origin-left group-hover:scale-100">{{icon.text}}</span>
+		</a>
+    </span>
+</div>
+`
 }
