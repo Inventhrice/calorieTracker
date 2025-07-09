a2c9f539bbb8d621252af1809a0788ed77feadcd
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2025-07-08 22:24:51 -0400

    feat: adding the login page

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-import { createApp, toRaw } from './vue.esm-browser.prod.js'
+import { createApp, toRaw } from './vue.esm-browser.js'
 import sidebaritem from '../components/sidebarItem.js'
 import contentHeader from '../components/headerComponent.js'
 import graphHeader from '../components/graphHeader.js'
@@ -23,3 +23,24 @@ createApp({
         }
     }
 }).mount('#settings')
+
+createApp({
+    data() {
+        return {
+            loginCreds: {email: "", password: ""},
+            errorMessage: {message: "", empty: true}
+        }
+    },
+    methods: {
+        async login(){
+            let response = await fetch("/api/profile/login", {method: "POST", body: JSON.stringify(this.loginCreds)})
+            if(!response.ok){
+                let resText = await response.text()
+                this.errorMessage.message = "Error: " + resText;
+                this.errorMessage.empty = false
+            } else{
+                window.location.href = "/app/index.html"
+            }
+        }
+    }
+}).mount('#login')
