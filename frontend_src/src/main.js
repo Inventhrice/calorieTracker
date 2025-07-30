3b1a8419369b45fa8cb174015b4a72594313eea1
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2025-07-30 13:19:15 -0400

    feat: adding a service worker along with a manifest in order to facilitate PWA

--- frontend_src/src/main.js
+++ frontend_src/src/main.js
@@ -3,3 +3,23 @@ import './index.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/serviceworker.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+registerServiceWorker();
