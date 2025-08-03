70c57e731226d24143b33b33ba867a8b3fc7cfcc
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2025-08-02 21:51:52 -0400

    fix: long overdue rehaul of the settings UI. no wonder i pushed this back to a v4

--- frontend_src/src/components/settings.vue
+++ frontend_src/src/components/settings.vue
@@ -4,8 +4,14 @@ export default {
     data() {
         return {
             title: "Settings",
-            loggedInUser: { firstname: "", lastname: "", pronouns: "", username: "", password: "", repeatpwd: "" },
-            errorMessage: ""
+            loggedInUser: { "First Name": "", "Last Name": "", "Pronouns": "", "Username": ""},
+            loginpwd:  {"Password": "", "Repeat Password": "" },
+            mealTimes: ["Breakfast", "Lunch", "Dinner", "Snacks"],
+            userGoals: { "Weight": 0, "% Error": 0, "Multiplier": 0 },
+            mealGoals: { breakfast: 0, lunch: 0, dinner: 0, snacks: 0 },
+            macroGoals: { calories: 0, protein: 0, fat: 0, carbs: 0 },
+            errorMessage: "",
+            changepwd: false
         }
     },
     methods: {
@@ -18,12 +24,12 @@ export default {
             }
         },
         async changePassword() {
-            let user = this.loggedInUser
-            if (user.password === user.repeatpwd) {
-                let response = await api_call("/api/profile/password", "PATCH", JSON.stringify({ "password": user.password }))
+            let user = this.loginpwd
+            if (user["Password"] === user["Repeat Password"]) {
+                let response = await api_call("/api/profile/password", "PATCH", JSON.stringify({ "password": user["Password"] }))
                 if (response.ok) {
-                    user.password = ""
-                    user.repeatpwd = ""
+                    user["Password"] = ""
+                    user["Repeat Password"] = ""
                 } else {
                     let msg = await response.text()
                     console.log(msg)
@@ -40,36 +46,53 @@ export default {
 </script>
 
 <template>
-    <div class="content-list">
-        <div class="module-background rounded-2xl flex flex-col">
-            <span class="mx-2"><label for="firstname">Firstname: </label><input v-model="loggedInUser.firstname"
-                    class="dialog-input m-3" type="text"></span>
-            <span class="mx-2"><label for="lastname">Lastname: </label><input v-model="loggedInUser.lastname"
-                    class="dialog-input m-3" type="text"></span>
-            <span class="mx-2"><label for="pronouns">Pronouns: </label><input v-model="loggedInUser.pronouns"
-                    class="dialog-input m-3" type="text"></span>
+    <div class="flex flex-row md:grid md:grid-cols-2 mt-2 ml-2 gap-y-3">
+        <div class="module-background rounded-xl w-fit flex flex-col">
+            <span class="informationSpan" v-for="info, key in loggedInUser">
+                <label :for="key">{{key}}:</label>
+                <input v-model="loggedInUser[key]" class="dialog-input grow my-3" type="text">
+            </span>
+            <span class="informationSpan flex-row my-2 justify-between gap-x-4">
+                <button class="btn btn-uhoh" @click="updateInformation">Save information</button>
+                <button class="btn" @click="changepwd = !changepwd">Change Password</button>
+            </span>
+
+            <div v-if="changepwd" class="">
+                <span class="informationSpan" v-for="data, key in loginpwd">
+                    <label :for="key">{{key}}: </label>
+                    <input v-model="loginpwd[key]" class="dialog-input grow my-2" type="password">
+                </span>
+                <span class="m-2 flex justify-between">
+                    <span>{{ errorMessage }}</span>
+                    <button class="btn btn-confirm" @click="changePassword">Save Password</button>
+                </span>
+            </div>
         </div>
-        <div class="module-background rounded-2xl flex flex-col">
-            <span class="mx-2">
-                <label for="username">Username: </label><input v-model="loggedInUser.username" class="dialog-input my-2"
-                    type="text">
-            </span>
-            <span class="mx-2">
-                <label for="password">New Password: </label>
-                <input v-model="loggedInUser.password" class="dialog-input my-2" type="password">
-            </span>
-            <span class="mx-2">
-                <label for="password">Reenter Password: </label>
-                <input v-model="loggedInUser.repeatpwd" class="dialog-input my-2" type="password">
-            </span>
-            <span class="m-2 flex justify-between">
-                <span>{{ errorMessage }}</span>
-                <button class="btn btn-confirm" @click="changePassword">Save</button>
-            </span>
-        </div>
-        <div class="module-background rounded-2xl flex flex-col">
-            <span class="m-2"><label for="goalsData">JSON Data for Goals:</label><textarea
-                    class="w-full dialog-input my-3"></textarea></span>
+        <div class="module-background rounded-xl justify-self-center grid grid-cols-2">
+            <span class="text-xl py-2 place-content-center text-center col-span-2">Goals Information</span>
+
+            <div class="flex flex-col">
+                <span v-for="goals, key in userGoals" :key class="informationSpan">
+                    <label for="key"> {{ key }}</label>
+                    <input type="text" class="dialog-input grow my-2" v-model="goals[key]">
+                </span>
+            </div>
+
+            <!-- TEMP -->
+            <div class="flex flex-col">
+                <span v-for="goals, key in userGoals" :key class="informationSpan">
+                    <label for="key"> {{ key }}</label>
+                    <input type="text" class="dialog-input grow my-2" v-model="goals[key]">
+                </span>
+            </div>
         </div>
     </div>
 </template>
+
+<style>
+@import "tailwindcss";
+
+.informationSpan {
+    @apply flex mx-2 place-items-center;
+}
+</style>
