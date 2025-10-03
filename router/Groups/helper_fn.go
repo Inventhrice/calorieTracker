dcf6224eee8dcabbca355af2925edc0cc84deff2
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2025-10-03 13:19:46 -0400

    unused: making some generic functions that are probably not going to get used

--- router/Groups/helper_fn.go
+++ router/Groups/helper_fn.go
@@ -3,9 +3,11 @@ package groups
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 
+	"example.com/m/v2/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -54,4 +56,61 @@ func Helper_ExecError(r sql.Result, initerr error, noRowsFound_errMsg string) (i
 	}
 
 	return rowsAffected, nil
+}
+
+// func delete(ctx *gin.Context, T DBStruct, errs ErrMsg, idParam string, tableName string) error {
+// 	id := helper_getIntFromStr(ctx, idParam, "Invalid entry ID")
+// 	result, err := middlewares.Database.Exec("DELETE FROM "+tableName+" WHERE id = ?", id)
+// 	if _, err := Helper_ExecError(result, err, errs.ExecError); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+// 		return err
+// 	}
+
+// 	ctx.Status(http.StatusOK)
+// 	return nil
+// }
+
+func addUpdate(ctx *gin.Context, T DBStruct, errs ErrMsg, query string, isPatch bool) error {
+	id := helper_getIntFromStr(ctx, T.GetIDKey(), errs.InvalidID)
+	if err := ctx.BindJSON(&T); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return err
+	}
+
+	if string(id) != T.GetID() && isPatch {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Error": fmt.Sprintf("IDs do not match, aborting request. obj id: %v | request id: %v", id, T.GetID())})
+	}
+
+	result, err := middlewares.Database.NamedExec(query, T)
+	if _, err := Helper_ExecError(result, err, errs.ExecError); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return err
+	}
+
+	if isPatch {
+		ctx.Status(http.StatusOK)
+	} else {
+		newId, _ := result.LastInsertId()
+		ctx.JSON(http.StatusOK, gin.H{"message": "Added successfully", "addedID": newId})
+	}
+
+	return nil
+}
+
+func get(ctx *gin.Context, query string, T DBStruct) error {
+	if err := middlewares.Database.Get(&T, query); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return err
+	}
+	ctx.JSON(http.StatusOK, T)
+	return nil
+}
+
+func getAll(ctx *gin.Context, query string, T []DBStruct) error {
+	if err := middlewares.Database.Select(&T, query); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+		return err
+	}
+	ctx.JSON(http.StatusOK, T)
+	return nil
 }
