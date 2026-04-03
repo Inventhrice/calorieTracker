3f77a59643a1c213a01318f74234c067dd438c0e
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2026-04-02 20:29:20 -0400

    Updating entries-dialog to 2 decimal places

--- frontend_src/src/components/entries/entries-dialog.vue
+++ frontend_src/src/components/entries/entries-dialog.vue
@@ -42,10 +42,10 @@ export default {
             if (this.isFoodID && this.foodinfo) {
                 let foodinfo = this.foodinfo
                 this.selected.foodname = foodinfo.name
-                this.selected.cal = foodinfo.calperg * quantity
-                this.selected.carbs = foodinfo.carbperg * quantity
-                this.selected.protein = foodinfo.proteinperg * quantity
-                this.selected.fat = foodinfo.fatperg * quantity
+                this.selected.cal = (foodinfo.calperg * quantity).toFixed(2)
+                this.selected.carbs = (foodinfo.carbperg * quantity).toFixed(2)
+                this.selected.protein = (foodinfo.proteinperg * quantity).toFixed(2)
+                this.selected.fat = (foodinfo.fatperg * quantity).toFixed(2)
             }
 
         }
