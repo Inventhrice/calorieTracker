9ab635a9a0a0b222a8ee61e27475b58a497227d5
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2026-07-14 19:07:30 -0400

    Updated goals.ts to use the new NutrientStats

--- frontend_src/src/components/settings/goals.ts
+++ frontend_src/src/components/settings/goals.ts
@@ -1,4 +1,4 @@
-import type { NutrientStats } from "../entries/entry.ts"
+import { NutrientStats } from "../entries/entry.ts"
 import { MealTimes } from '../entries/entry.ts'
 
 export function calc_day_goals(goalinfo: { multiplier: number, goalLbs: number, proteinGPerLBS: number, fatGPerLBS: number }): NutrientStats {
@@ -6,12 +6,12 @@ export function calc_day_goals(goalinfo: { multiplier: number, goalLbs: number, 
     const PROTEIN_CALPERGRAM = 4
     const CARB_CALPERGRAM = 4
 
-    let day_nutrients: NutrientStats = {
-        cal: goalinfo.goalLbs * goalinfo.multiplier,
-        protein: Math.round(goalinfo.proteinGPerLBS * goalinfo.goalLbs),
-        fat: Math.round(goalinfo.fatGPerLBS * goalinfo.goalLbs),
-        carbs: 0
-    }
+    let day_nutrients: NutrientStats = new NutrientStats(
+        goalinfo.goalLbs * goalinfo.multiplier,
+        Math.round(goalinfo.proteinGPerLBS * goalinfo.goalLbs),
+        Math.round(goalinfo.fatGPerLBS * goalinfo.goalLbs),
+        0
+    )
 
     day_nutrients.carbs = (day_nutrients.cal - (day_nutrients.fat * FAT_CALPERGRAM) + (day_nutrients.protein * PROTEIN_CALPERGRAM)) / CARB_CALPERGRAM
     return day_nutrients
@@ -19,12 +19,12 @@ export function calc_day_goals(goalinfo: { multiplier: number, goalLbs: number, 
 
 // Applicable for calculating per meal goals AND tolerances
 export function calc_pct_goals(totals: NutrientStats, pct: number): NutrientStats {
-    let nutrients: NutrientStats = {
-        cal: Math.round(totals.cal * pct),
-        protein: Math.round(totals.protein * pct),
-        fat: Math.round(totals.fat * pct),
-        carbs: Math.round(totals.carbs * pct)
-    }
+    let nutrients: NutrientStats = new NutrientStats(
+        Math.round(totals.cal * pct),
+        Math.round(totals.protein * pct),
+        Math.round(totals.fat * pct),
+        Math.round(totals.carbs * pct)
+    )
     return nutrients
 }
 
@@ -36,8 +36,8 @@ export function parse_goals(goalinfo: { "goalLbs": number, "multiplier": number,
     let day_goals = calc_day_goals(goalinfo)
     let err_daygoals = calc_pct_goals(day_goals, goalinfo.acceptablePercent)
 
-    all_calculated["totals"] = { "day": day_goals }
-    all_calculated["marginOfError"] = { "day": err_daygoals }
+    all_calculated["totals"] = { "Total": day_goals }
+    all_calculated["marginOfError"] = { "Total": err_daygoals }
 
     for (let index in MealTimes) {
         let meal = MealTimes[index]
